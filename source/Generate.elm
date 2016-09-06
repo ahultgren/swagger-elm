f5144ed16709bb15c9f4ea7e8c97298b2bccf9c6
Andreas Hultgren <me@andreashultgren.se>
2016-09-06 18:10:02 +0200

    Add CLI support and refactor example generation

--- source/Generate.elm
+++ source/Generate.elm
@@ -1,14 +1,41 @@
-module Generate exposing (..)
+module Generate exposing (main, generate)
 
 import String
 import Dict
 import Regex exposing (regex)
+import Html exposing (text)
+import Html.App exposing (programWithFlags)
 import Json.Decode exposing (decodeString)
 import Swagger.Decode as Swagger exposing (Swagger, decodeSwagger)
 import Swagger.Parse exposing (parseDefinitions)
 import Generate.Type as Type
 import Generate.Decoder as Decoder
 import Generate.Headers as Headers
+
+
+main : Program String
+main =
+    programWithFlags
+        { init = init
+        , view = text << toOutput << generate
+        , update = always << always ( "", Cmd.none )
+        , subscriptions = always Sub.none
+        }
+
+
+init : String -> ( String, Cmd x )
+init json =
+    ( json, Cmd.none )
+
+
+toOutput : Result String String -> String
+toOutput result =
+    case result of
+        Err err ->
+            Debug.log "error" err
+
+        Ok ok ->
+            ok
 
 
 generate : String -> Result String String
